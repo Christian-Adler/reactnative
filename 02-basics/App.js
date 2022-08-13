47f5b64da5400ea498dd42b13904343eb9d0a4bd
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-08-13 07:56:58 +0200

    view layout flexbox

--- 02-basics/App.js
+++ 02-basics/App.js
@@ -1,39 +1,33 @@
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View>
-        <Text style={styles.dummyText}>Hallo</Text>
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Your goal!" style={styles.textInput} />
+        <Button title="Add Goal" />
       </View>
-      <Text style={styles.dummyText}>Hallo2</Text>
-      <Button title="Tap me" />
-      <View style={styles.rounded}></View>
+      <View>
+        <Text>List of Goals</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
+    padding: 50,
+  },
+
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#ccc",
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-
-  dummyText: {
-    margin: 16,
-    borderWidth: 2,
-    borderColor: "#f0d",
-    padding: 16,
-  },
-
-  rounded: {
-    margin: 16,
-    borderWidth: 2,
-    borderColor: "#00a",
-    height: 50,
-    width: 160,
-    borderRadius: 15,
+    marginRight: 8,
+    padding: 8,
   },
 });
