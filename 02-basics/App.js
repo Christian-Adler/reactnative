69cecc5bac44b832a749a0b161007fc114b945bd
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-08-13 07:11:48 +0200

    added more components

--- 02-basics/App.js
+++ 02-basics/App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!!!!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text>Hallo</Text>
+      </View>
+      <Text>Hallo2</Text>
+      <Button title="Tap me" />
     </View>
   );
 }
@@ -13,8 +15,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
