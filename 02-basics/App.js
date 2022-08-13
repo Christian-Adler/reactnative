ba35aaa6b3e75ea46eb9675e403760e772fdeed6
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-08-13 20:37:20 +0200

    output as list

--- 02-basics/App.js
+++ 02-basics/App.js
@@ -2,13 +2,16 @@ import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 import { useState } from "react";
 
 export default function App() {
+  const [goals, setGoals] = useState([]);
   const [enteredGoalText, setEnteredGoalText] = useState("");
+
   const goalInputHandler = (enteredText) => {
     // console.log(enteredText);
     setEnteredGoalText(enteredText);
   };
   const addGoalHandler = () => {
-    console.log(enteredGoalText);
+    setGoals((prevState) => [...prevState, enteredGoalText]);
+    setEnteredGoalText("");
   };
 
   return (
@@ -22,7 +25,9 @@ export default function App() {
         <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        <Text>List of Goals</Text>
+        {goals.map((goal) => (
+          <Text key={goal}>{goal}</Text>
+        ))}
       </View>
     </View>
   );
