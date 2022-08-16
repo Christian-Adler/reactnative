6a4fb7a41735d39340d5a2201bf0204ce734ec42
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-08-16 14:32:33 +0200

    colors managed globally

--- 04-components-layout-styling/App.js
+++ 04-components-layout-styling/App.js
@@ -10,6 +10,7 @@ import { Fragment, useState } from "react";
 import StartGameScreen from "./screens/StartGameScreen";
 import { LinearGradient } from "expo-linear-gradient";
 import GameScreen from "./screens/GameScreen";
+import Colors from "./constants/colors";
 
 export default function App() {
   const [userNumber, setUserNumber] = useState(undefined);
@@ -25,7 +26,10 @@ export default function App() {
   return (
     <Fragment>
       <StatusBarExpo style="light" />
-      <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.rootScreen}>
+      <LinearGradient
+        colors={[Colors.primary700, Colors.accent500]}
+        style={styles.rootScreen}
+      >
         <ImageBackground
           source={require("./assets/images/background.png")}
           resizeMode="cover"
--- 04-components-layout-styling/screens/StartGameScreen.js
+++ 04-components-layout-styling/screens/StartGameScreen.js
@@ -1,6 +1,7 @@
 import { Alert, StyleSheet, TextInput, View } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 import { useState } from "react";
+import Colors from "../constants/colors";
 
 const StartGameScreen = ({ onPickNumber }) => {
   const [enteredNumber, setEnteredNumber] = useState("");
@@ -57,7 +58,7 @@ const styles = StyleSheet.create({
     marginTop: 100,
     padding: 16,
     marginHorizontal: 24,
-    backgroundColor: "#3b021f",
+    backgroundColor: Colors.primary800,
     borderRadius: 8,
     elevation: 4, // android
     shadowColor: "black", // iOS
@@ -72,9 +73,9 @@ const styles = StyleSheet.create({
     width: 50,
     textAlign: "center",
     fontSize: 32,
-    borderBottomColor: "#ddb52f",
+    borderBottomColor: Colors.accent500,
     borderBottomWidth: 2,
-    color: "#ddb52f",
+    color: Colors.accent500,
     marginVertical: 8,
     fontWeight: "bold",
   },
