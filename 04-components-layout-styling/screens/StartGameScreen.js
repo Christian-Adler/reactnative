a34341aa9f98ec4b5193fe4790122f0d1515b492
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-08-16 09:02:23 +0200

    styled input container with shadow os specific

--- 04-components-layout-styling/screens/StartGameScreen.js
+++ 04-components-layout-styling/screens/StartGameScreen.js
@@ -1,9 +1,9 @@
-import { TextInput, View } from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 const StartGameScreen = (props) => {
   return (
-    <View>
+    <View style={styles.inputContainer}>
       <TextInput />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
@@ -12,3 +12,18 @@ const StartGameScreen = (props) => {
 };
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    padding: 16,
+    marginHorizontal: 24,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+    elevation: 4, // android
+    shadowColor: "black", // iOS
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+});
