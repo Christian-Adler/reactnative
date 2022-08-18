a5fa9347172f1127a0ac7d142e319d27c5b3d4b6
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-08-18 13:22:18 +0200

    added route-Hooks comment

--- 06-navigation/screens/MealsOverviewScreen.js
+++ 06-navigation/screens/MealsOverviewScreen.js
@@ -1,9 +1,13 @@
 import { StyleSheet, Text, View } from "react-native";
+//import {useRoute} from "@react-navigation/native";
 
 const MealsOverviewScreen = ({ /*navigation,*/ route }) => {
   // https://reactnavigation.org/docs/navigation-prop
   // https://reactnavigation.org/docs/route-prop
   const { categoryId: catId } = route.params;
+
+  // route-Objekt per Hook waere in allen Komponenten erreichbar
+  // const routeObj = useRoute();
 
   return (
     <View style={styles.container}>
