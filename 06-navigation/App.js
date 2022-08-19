658f34a200cee6063a1801eb23b0b950b48d53ab
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-08-19 10:46:23 +0200

    added icons

--- 06-navigation/App.js
+++ 06-navigation/App.js
@@ -9,6 +9,7 @@ import MealDetailScreen from "./screens/MealDetailScreen";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import CategoriesScreen from "./screens/CategoriesScreen";
 import FavoritesScreen from "./screens/FavoritesScreen";
+import { Ionicons } from "@expo/vector-icons";
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -20,14 +21,31 @@ const DrawerNavigator = () => {
         headerStyle: { backgroundColor: "#351401" },
         headerTintColor: "white",
         sceneContainerStyle: { backgroundColor: "#3f2f25" },
+        drawerContentStyle: { backgroundColor: "#351401" },
+        drawerInactiveTintColor: "white",
+        drawerActiveTintColor: "#351401",
+        drawerActiveBackgroundColor: "#e4baa1",
       }}
     >
       <Drawer.Screen
         name={NAV.MealsCategories}
         component={CategoriesScreen}
-        options={{ title: "All Categories" }}
+        options={{
+          title: "All Categories",
+          drawerIcon: ({ color, size }) => {
+            return <Ionicons name={"list"} color={color} size={size} />;
+          },
+        }}
       />
-      <Drawer.Screen name={NAV.Favorites} component={FavoritesScreen} />
+      <Drawer.Screen
+        name={NAV.Favorites}
+        component={FavoritesScreen}
+        options={{
+          drawerIcon: ({ color, size }) => {
+            return <Ionicons name={"star"} color={color} size={size} />;
+          },
+        }}
+      />
     </Drawer.Navigator>
   );
 };
