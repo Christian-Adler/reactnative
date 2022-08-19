c121baa1253889ceeb4dac200eb812ce175f9c68
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-08-19 09:15:31 +0200

    added nested navigator

--- 06-navigation/App.js
+++ 06-navigation/App.js
@@ -1,14 +1,36 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
-import CategoriesScreen from "./screens/CategoriesScreen";
 import { Fragment } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import NAV from "./constants/navigation";
 import MealDetailScreen from "./screens/MealDetailScreen";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import CategoriesScreen from "./screens/CategoriesScreen";
+import FavoritesScreen from "./screens/FavoritesScreen";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const DrawerNavigator = () => {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: "#351401" },
+        headerTintColor: "white",
+        sceneContainerStyle: { backgroundColor: "#3f2f25" },
+      }}
+    >
+      <Drawer.Screen
+        name={NAV.MealsCategories}
+        component={CategoriesScreen}
+        options={{ title: "All Categories" }}
+      />
+      <Drawer.Screen name={NAV.Favorites} component={FavoritesScreen} />
+    </Drawer.Navigator>
+  );
+};
 
 export default function App() {
   return (
@@ -23,11 +45,11 @@ export default function App() {
           }}
         >
           <Stack.Screen
-            name={NAV.MealsCategories}
+            name={NAV.DrawerNavigator}
             options={{
-              title: "All Categories",
+              headerShown: false,
             }}
-            component={CategoriesScreen}
+            component={DrawerNavigator}
           />
           <Stack.Screen
             name={NAV.MealsOverview}
--- 06-navigation/constants/navigation.js
+++ 06-navigation/constants/navigation.js
@@ -2,9 +2,11 @@
  * @type {{MealsCategories: string, MealsOverview: string}}
  */
 const NAV = {
+  DrawerNavigator: "DrawerNavigator",
   MealsCategories: "MealsCategories",
   MealsOverview: "MealsOverview",
   MealDetail: "MealDetail",
+  Favorites: "Favorites",
 };
 
 export default NAV;
