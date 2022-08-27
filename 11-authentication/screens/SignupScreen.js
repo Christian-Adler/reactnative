818d49ee559d5fb6951e96153fe695eb2b51bd86
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-08-27 11:37:40 +0200

    added handling errors

--- 11-authentication/screens/SignupScreen.js
+++ 11-authentication/screens/SignupScreen.js
@@ -1,13 +1,18 @@
 import {authenticate, createUser} from "../util/auth";
 import {useState} from "react";
 import TransparentLoadingOverlay from "../components/ui/TransparentLoadingOverlay";
+import {Alert} from "react-native";
 
 function SignupScreen() {
   const [isAuthenticating, setIsAuthenticating] = useState(false);
   
   const signupHandler = async ({ email, password }) => {
     setIsAuthenticating(true);
-    await createUser(email, password);
+    try {
+      await createUser(email, password);
+    } catch (e) {
+      Alert.alert('Authentication failed', 'Could not create user. Check input or try again later.')
+    }
     setIsAuthenticating(false);
   }
   
