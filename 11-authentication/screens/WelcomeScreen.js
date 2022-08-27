427757a0945fe26e3bc7dc9e34d2e547b64b6156
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-08-27 14:02:31 +0200

    added API request with token access

--- 11-authentication/screens/WelcomeScreen.js
+++ 11-authentication/screens/WelcomeScreen.js
@@ -1,10 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import {useContext, useEffect, useState} from "react";
+import axios from "axios";
+import {AuthContext} from "../store/auth-context";
 
 function WelcomeScreen() {
+  const [fetchedMessage, setFetchedMessage] = useState();
+  const authCtx = useContext(AuthContext);
+  const token = authCtx.token;
+  
+  useEffect(() => {
+    axios.get('https://react-native-38ace-default-rtdb.europe-west1.firebasedatabase.app/message.json?auth=' + token).then((response) => {
+      // console.log(response);
+      setFetchedMessage(response.data);
+    })
+  }, [token]);
+  
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
