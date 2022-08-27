e054e71d68fe12ff994e1fa495bffa0bd895045c
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-08-27 07:39:57 +0200

    added axios to project and auth helper for firebase backend

---
+++ 11-authentication/util/auth.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const API_KEY = 'AIzaSyAqnwcFihpiMJywHkkRxIWH9YcykFEySis';
+
+export const createUser = async (email, password) => {
+  const response = await axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`, {
+    email,
+    password,
+    returnSecureToken: true,
+  });
+};
