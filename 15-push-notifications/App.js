0429c72026a24b3c5e732915d6310f30bb3fa3d8
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-09-01 17:12:35 +0200

    added first local notification

--- 15-push-notifications/App.js
+++ 15-push-notifications/App.js
@@ -1,10 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, View } from "react-native";
+import * as Notifications from "expo-notifications";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldPlaySound: false,
+      shouldSetBadge: false,
+      shouldShowAlert: true,
+    };
+  },
+});
 
 export default function App() {
+  const scheduleNotificationHandler = async () => {
+    Notifications.scheduleNotificationAsync({
+      content: {
+        // https://docs.expo.dev/versions/latest/sdk/notifications/#notificationcontentinput
+        title: "My first local notification",
+        subtitle: "My first subtitle for notification",
+        body: "This is the body of the notification!",
+        data: { userName: "Chris" }, // data to handle when using this notification
+      },
+      trigger: {
+        // https://docs.expo.dev/versions/latest/sdk/notifications/#notificationtriggerinput
+        seconds: 5,
+      },
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Hello World!!!!</Text>
+      <Button
+        title={"Schedule Notification"}
+        onPress={scheduleNotificationHandler}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +43,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
