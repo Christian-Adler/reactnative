540a4d630af9862582422716912e14715cef892b
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-09-01 20:44:45 +0200

    added eventlisteners to notification

--- 15-push-notifications/App.js
+++ 15-push-notifications/App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { Button, StyleSheet, View } from "react-native";
 import * as Notifications from "expo-notifications";
+import { useEffect } from "react";
 
 Notifications.setNotificationHandler({
   handleNotification: async () => {
@@ -13,6 +14,35 @@ Notifications.setNotificationHandler({
 });
 
 export default function App() {
+  console.log("APP RENDER");
+  useEffect(() => {
+    console.log("In useEffect");
+    // Auf Notification hoeren, wenn App geoeffnet ist - der User muss nicht auf die Notification klicken.
+    const subscription = Notifications.addNotificationReceivedListener(
+      (notification) => {
+        console.log("NOTIFICATION RECEIVED");
+        console.log(notification);
+        const userName = notification.request.content.data.userName;
+        console.log(userName);
+      }
+    );
+
+    // User has to respond (tab) on the notification
+    const subscription2 = Notifications.addNotificationResponseReceivedListener(
+      (response) => {
+        console.log("NOTIFICATION RESPONSE RECEIVED");
+        console.log(response);
+        const userName = response.notification.request.content.data.userName;
+        console.log(userName);
+      }
+    );
+
+    return () => {
+      subscription.remove();
+      subscription2.remove();
+    };
+  }, []);
+
   const scheduleNotificationHandler = async () => {
     Notifications.scheduleNotificationAsync({
       content: {
@@ -20,7 +50,7 @@ export default function App() {
         title: "My first local notification",
         subtitle: "My first subtitle for notification",
         body: "This is the body of the notification!",
-        data: { userName: "Chris" }, // data to handle when using this notification
+        data: { userName: "Chris" }, // data to handle when using this notification - not direct visible to the user
       },
       trigger: {
         // https://docs.expo.dev/versions/latest/sdk/notifications/#notificationtriggerinput
